a7e3b44285e57728b0d15d5a15fc9d1e58180ce1
Martin Olmr <info@martinolmr.cz>
2021-02-25 12:06:08 +0100

    fix missing entity directory and bootstrap

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -30,10 +30,10 @@ class Bootstrap
 		$configurator = new Configurator;
 
 		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy($appDir . '/log');
+		$configurator->enableTracy($appDir . '/../log');
 
 		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory($appDir . '/temp');
+		$configurator->setTempDirectory($appDir . '/../temp');
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
