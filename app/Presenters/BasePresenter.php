1368c1a5d6fe849a56f4dc59c75f139a3be77b49
Martin Olmr <info@martinolmr.cz>
2021-04-14 10:55:35 +0200

    update templates

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -5,34 +5,67 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Contributte\Translation\LocalesResolvers\Session;
-use Nette;
+use Nette\Application\UI\Presenter;
+use Nette\Localization\Translator;
+use Nette\Application\AbortException;
 
 
 /**
  * Base presenter for all application presenters.
  */
-abstract class BasePresenter extends Nette\Application\UI\Presenter
+abstract class BasePresenter extends Presenter
 {
 	/**
 	 * @var string
 	 * @persistent
 	 */
-	public $locale;
+	public string $locale = 'cs';
 
-	/** @var Nette\Localization\ITranslator @inject */
-	public $translator;
+	/**
+	 * @var Translator
+	 * @inject
+	 */
+	public Translator $translator;
 
-	/** @var Session @inject */
-	public $translatorSessionResolver;
+	/**
+	 * @var Session
+	 * @inject
+	 */
+	public Session $translatorSessionResolver;
 
 	/**
 	 * @param string $locale
-	 * @throws Nette\Application\AbortException
+	 * @throws AbortException
 	 */
 	public function handleChangeLocale(string $locale): void
 	{
 		$this->translatorSessionResolver->setLocale($locale);
 		$this->redirect('this');
+	}
+
+	/**
+	 * @param string $locale
+	 * @return bool
+	 */
+	public function isLocale(string $locale): bool
+	{
+		return $this->locale === $locale;
+	}
+
+	/**
+	 * @return array<string>
+	 */
+	public function getLocales(): array
+	{
+		return $this->translator->getLocalesWhitelist();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDefaultLocale(): string
+	{
+		return $this->translator->getDefaultLocale();
 	}
 
 }
